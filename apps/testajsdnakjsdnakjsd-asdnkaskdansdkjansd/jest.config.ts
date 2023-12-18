04d95aac9a89126ae78811cf2c73b08beb915536
Charlie Greenman <charlie@razroo.com>
2023-12-18 13:10:02 -0500

    Add code for angular-17.0.0 create-workspace.
    [Url: razroo.com/community/angular-17.0.0/create-workspace-recipe/create-workspace]

---
+++ apps/testajsdnakjsdnakjsd-asdnkaskdansdkjansd/jest.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export default {
+  displayName: 'testajsdnakjsdnakjsd-asdnkaskdansdkjansd',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {},
+  coverageDirectory: '../../coverage/apps/testajsdnakjsdnakjsd-asdnkaskdansdkjansd',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
